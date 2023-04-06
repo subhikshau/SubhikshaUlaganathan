3623eb713d07b00a2b6aca5549d80f8d9bd83c6e
Subhiksha Ulaganathan <97969342+subhikshaulaganathan@users.noreply.github.com>
2023-04-06 13:41:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+![subhiksha](https://user-images.githubusercontent.com/110616069/230312143-99a12536-5086-4fb7-86bc-8984338a3492.jpg)
---
+++ script.js
@@ -0,0 +1,8 @@
+    var typed = new Typed(".typing", {
+        strings: ["Engineer","Developer", "Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
